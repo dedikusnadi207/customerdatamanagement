8dd21b018f75bb3519a9218103d88a572f20f2c7
T. Yulis <timuryulis14@gmail.com>
2023-06-18 07:53:46 +0700

    Create cutomerdatamanagement-FULL-DB.sql

---
+++ cutomerdatamanagement-FULL-DB.sql
@@ -0,0 +1,121 @@
+/*
+SQLyog Ultimate v12.5.1 (64 bit)
+MySQL - 10.4.22-MariaDB : Database - customerdatamanagement
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`customerdatamanagement` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `customerdatamanagement`;
+
+/*Table structure for table `admin` */
+
+CREATE TABLE `admin` (
+  `id_admin` int(3) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `informasi_perusahaan` */
+
+CREATE TABLE `informasi_perusahaan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_perusahaan` varchar(50) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  `no_tlp` varchar(13) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `logo` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `jadwal_sales` */
+
+CREATE TABLE `jadwal_sales` (
+  `id_jadwal` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sales` int(3) DEFAULT NULL,
+  `id_pelanggan` int(5) DEFAULT NULL,
+  `kegiatan` text DEFAULT NULL,
+  `waktu` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_jadwal`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `layanan` */
+
+CREATE TABLE `layanan` (
+  `id_layanan` int(5) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) DEFAULT NULL,
+  `deskripsi` varchar(50) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_layanan`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `pelanggan` */
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(5) NOT NULL AUTO_INCREMENT,
+  `nik` varchar(20) DEFAULT NULL,
+  `nama_pic` varchar(50) DEFAULT NULL,
+  `nama_instansi` varchar(50) DEFAULT NULL,
+  `no_tlp` varchar(13) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_pelanggan`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `pembayaran` */
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT,
+  `id_transaksi` int(11) DEFAULT NULL,
+  `jumlah_pembayaran` int(11) DEFAULT NULL,
+  `tgl_bayar` date DEFAULT NULL,
+  `keterangan` varchar(255) DEFAULT NULL,
+  `id_admin` int(11) NOT NULL,
+  PRIMARY KEY (`id_pembayaran`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `sales` */
+
+CREATE TABLE `sales` (
+  `id_sales` int(3) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_sales`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `transaksi` */
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL AUTO_INCREMENT,
+  `tanggal_mulai` datetime DEFAULT NULL,
+  `tanggal_selesai` datetime DEFAULT NULL,
+  `id_pelanggan` int(5) DEFAULT NULL,
+  `id_layanan` int(5) DEFAULT NULL,
+  `id_sales` int(3) DEFAULT NULL,
+  `total_harga` int(11) DEFAULT NULL,
+  `status` varchar(20) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_transaksi`),
+  KEY `id_pelanggan` (`id_pelanggan`),
+  KEY `id_layanan` (`id_layanan`),
+  KEY `id_sales` (`id_sales`),
+  CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`),
+  CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id_layanan`),
+  CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`id_sales`) REFERENCES `sales` (`id_sales`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
